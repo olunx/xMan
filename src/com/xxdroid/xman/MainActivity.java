27b0b356410e02beafc8e0791c8b04c4bcde1ae2
lunzii <lunzii@gmail.com>
2013-10-24 19:01:44 +0800

    增加xUtils

--- src/com/xxdroid/xman/MainActivity.java
+++ src/com/xxdroid/xman/MainActivity.java
@@ -3,12 +3,13 @@ package com.xxdroid.xman;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity
-{
-    /** Called when the activity is first created. */
+public class MainActivity extends Activity {
+
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 }
