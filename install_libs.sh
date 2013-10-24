74b8a85196488ab69f1ddbad6622a23b2305bd32
lunzii <lunzii@gmail.com>
2013-10-24 19:01:12 +0800

    更新复制脚本

--- install_libs.sh
+++ install_libs.sh
@@ -13,7 +13,7 @@ echo "----------Install so--------------"
 for S in libiconv.so libxml2.so libplist.so libplist++.so libusb.so libusbmuxd.so libcrypto.so libssl.so libimobiledevice.so libzip.so
 do
   rm "$LIBS_DIR/$S"
-  mv "$SRC_DIR/$S" $LIBS_DIR
+  cp "$SRC_DIR/$S" $LIBS_DIR
 done
 
 # Install binary
@@ -21,7 +21,7 @@ echo "----------Install binary--------------"
 for B in listdevs openssl usbmuxdd ideviceid ideviceinfo idevicedate idevicediagnostics idevicescreenshot idevicesyslog ideviceinstaller ifuse fusermount
 do
   rm "$BIN_DIR/$B"
-  mv "$SRC_DIR/$B" $BIN_DIR
+  cp "$SRC_DIR/$B" $BIN_DIR
 done
 
 echo "----------Install done--------------"
