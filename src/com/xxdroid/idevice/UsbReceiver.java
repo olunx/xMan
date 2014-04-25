61f5d79395025754a03c362fb527645f8479534b
lunzii <lunzic@gmail.com>
2014-04-25 15:49:28 +0800

    更新应用架构

--- src/com/xxdroid/idevice/UsbReceiver.java
+++ src/com/xxdroid/idevice/UsbReceiver.java
@@ -3,17 +3,31 @@ package com.xxdroid.idevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
 import android.widget.Toast;
 
 import com.lidroid.xutils.util.LogUtils;
+import com.xxdroid.xman.HexDump;
 
 /**
- * Created by olunxchen on 2013.
+ * Created by olunxchen on 2014.
  */
 public class UsbReceiver extends BroadcastReceiver {
+
     @Override
     public void onReceive(Context context, Intent intent) {
         LogUtils.e("onReceive UsbReceiver-------------------------------------");
         Toast.makeText(context, "onReceive UsbReceiver", Toast.LENGTH_LONG).show();
+
+        UsbManager usbManager = (UsbManager)context.getSystemService(Context.USB_SERVICE);
+        for (UsbDevice device : usbManager.getDeviceList().values()){
+            String title = String.format("Vendor %s Product %s",
+                    HexDump.toHexString((short) device.getVendorId()),
+                    HexDump.toHexString((short) device.getProductId()));
+
+            LogUtils.e("found usb device: " + title);
+        }
     }
+
 }
--- src/com/xxdroid/xman/MainActivity.java
+++ src/com/xxdroid/xman/MainActivity.java
@@ -1,17 +1,9 @@
 package com.xxdroid.xman;
 
 import android.app.Activity;
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.hardware.usb.UsbDevice;
-import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -19,22 +11,11 @@ import com.lidroid.xutils.util.LogUtils;
 import com.stericson.RootTools.RootTools;
 import com.stericson.RootTools.exceptions.RootDeniedException;
 import com.stericson.RootTools.execution.Command;
-import com.stericson.RootTools.execution.CommandCapture;
-import com.xxdroid.idevice.DeviceDetector;
-import com.xxdroid.idevice.DeviceInfo;
-import com.xxdroid.idevice.DeviceManagerService;
-import com.xxdroid.idevice.UsbReceiver;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
 public class MainActivity extends Activity {
-
-    private String busybox = "busybox";
-    private String usbmuxd = "usbmuxdd";
-    private String ideviceid = "ideviceid";
-    private String ideviceinfo = "ideviceinfo";
 
     private TextView textView;
     private Handler handler;
@@ -61,45 +42,17 @@ public class MainActivity extends Activity {
         appPath = getApplicationInfo().dataDir;
         textView.setText("appPath: " + appPath + "\n");
 
-        LogUtils.e("new UsbReceiver-------------------------------------");
-        UsbReceiver usbReceiver = new UsbReceiver();
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED);
-        filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
-//        filter.addAction(UsbManager.ACTION_USB_ACCESSORY_ATTACHED);
-//        filter.addAction(UsbManager.ACTION_USB_ACCESSORY_DETACHED);
-        registerReceiver(usbReceiver, filter);
     }
 
     public void onClick(View view){
         switch (view.getId()){
-            case R.id.btnInstall:{
-                installBin();
-                textView.setText(textView.getText() + " binary installed" + " \n");
-                LogUtils.d("binary installed");
-                break;
-            }
-            case R.id.btnUsbmuxd:{
-                Command command = new XCommand(0,
-                        exportLib(),
-                        getBinPath(usbmuxd),
-                        getBinPath(busybox) + " ps | " + getBinPath(busybox) + " grep usbmuxd");
-                runCommand(command);
-                break;
-            }
             case R.id.btnIdeviceid:{
-                Command command = new XCommand(0,
-                        exportLib(),
-                        getBinPath(ideviceid + " -l")
-                );
+                Command command = new XCommand(0, " -l");
                 runCommand(command);
                 break;
             }
             case R.id.btnIdeviceinfo:{
-                Command command = new XCommand(0,
-                        exportLib(),
-                        getBinPath(ideviceinfo)
-                );
+                Command command = new XCommand(0, " -l");
                 runCommand(command);
                 break;
             }
@@ -145,25 +98,6 @@ public class MainActivity extends Activity {
         Message msg = new Message();
         msg.setData(bundle);
         handler.sendMessage(msg);
-    }
-
-    private void installBin(){
-        RootTools.installBinary(this, R.raw.usbmuxdd, usbmuxd);
-        RootTools.installBinary(this, R.raw.busybox_armv7, busybox);
-        RootTools.installBinary(this, R.raw.ideviceid, ideviceid);
-        RootTools.installBinary(this, R.raw.ideviceinfo, ideviceinfo);
-    }
-
-    private String exportLib(){
-        String result = "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:" + appPath + "/lib";
-        LogUtils.d(result);
-        return result;
-    }
-
-    private String getBinPath(String bin){
-        String path = appPath + "/files/" + bin;
-        LogUtils.d(path);
-        return path;
     }
 
     private void runCommand(Command command){
